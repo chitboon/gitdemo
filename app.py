b691978aaf3cf605f7145b457db7133d8062aadc
leecb <chitboon@gmail.com>
2019-12-13 08:53:56 +0800

    Added new route /user

--- app.py
+++ app.py
@@ -7,6 +7,9 @@ app = Flask(__name__)
 def home():
     return render_template('home.html')
 
+@app.route('/user')
+def user():
+    return render_template('home.html')
 
 if __name__ == '__main__':
     app.run()
